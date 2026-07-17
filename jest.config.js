8df4a3a2d5a64bce3998458e86fb553709d98727
StevieAdrian <adrianstevie0@gmail.com>
2026-07-18 02:41:32 +0700

    fix: native auth modules for jest

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,11 @@
 module.exports = {
   preset: '@react-native/jest-preset',
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|@react-navigation|react-native-screens|react-native-safe-area-context|react-native-css-interop|nativewind)/)',
+    'node_modules/(?!(react-native|@react-native|@react-native-google-signin|@react-native-async-storage|react-native-url-polyfill|@react-navigation|react-native-screens|react-native-safe-area-context|react-native-css-interop|nativewind)/)',
   ],
   moduleNameMapper: {
     '\\.css$': '<rootDir>/jest/styleMock.js',
+    '^@react-native-async-storage/async-storage$': '@react-native-async-storage/async-storage/jest',
   },
+  setupFiles: ['<rootDir>/node_modules/@react-native-google-signin/google-signin/jest/build/jest/setup.js'],
 };
