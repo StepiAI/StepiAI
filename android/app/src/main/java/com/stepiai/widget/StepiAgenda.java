d928bbcc3512cdcd2050cbe0ce08df9fce7fe01d
StevieAdrian <adrianstevie0@gmail.com>
2026-07-19 22:42:19 +0700

    feat: update dependency and widget

---
+++ android/app/src/main/java/com/stepiai/widget/StepiAgenda.java
@@ -0,0 +1,7 @@
+package com.stepiai.widget;
+
+import com.reactnativeandroidwidget.RNWidgetProvider;
+
+// provider buat widget, sengaja gw kosongin, extend nya udh nyediain logic yg di throw ke headless js
+public class StepiAgenda extends RNWidgetProvider {
+}
