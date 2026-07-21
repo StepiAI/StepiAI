0fbc363c1f80428e92f0c0dd780dea4f662af07f
StevieAdrian <adrianstevie0@gmail.com>
2026-07-22 03:13:24 +0700

    feat: adjust activeroute

--- src/app/navigation/RootNavigator.tsx
+++ src/app/navigation/RootNavigator.tsx
@@ -22,9 +22,13 @@ const Tab = createBottomTabNavigator<MainTabParamList>();
 function MainTabs() {
   const [addingSchedule, setAddingSchedule] = useState(false);
 
-  const renderTabBar = (props: BottomTabBarProps) => (
-    <TabBar {...props} onAddPress={() => setAddingSchedule(true)} />
-  );
+  const renderTabBar = (props: BottomTabBarProps) => {
+    const activeRoute = props.state.routes[props.state.index]?.name;
+    if (activeRoute === 'Chat') return null;
+    return (
+      <TabBar {...props} onAddPress={() => setAddingSchedule(true)} />
+    );
+  };
 
   return (
     <>
